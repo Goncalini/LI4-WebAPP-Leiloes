24871831585c4292bcde6435603fd2e5a528fb7b
Goncalini <goncalodaniel28@gmail.com>
2024-01-19 01:55:51 +0000

    Ja tou farto e ainda não começou

---
+++ .NET 6/DataLayer/ISqlDataAccess.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DataLayer
+{
+    public interface ISqlDataAccess
+    {
+        string ConnectionStringName { get; set; }
+
+        Task<List<T>> LoadData<T, U>(string sql, U parameters);
+        Task SaveData<T>(string sql, T parameters);
+    }
+}
