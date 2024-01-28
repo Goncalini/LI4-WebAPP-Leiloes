070d385b3e01331aceef5c543d69851ae929bc4e
MartaRodrigues03 <martarodrigues2003@gmail.com>
2024-01-28 23:59:43 +0000

    paginas criadas

--- .NET 6/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
+++ .NET 6/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ba81e94dd2fb05d1413693c4e75ad846908166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
