a9993070b89db45e62524bc87bfeec831b6b2a11
Goncalini <goncalodaniel28@gmail.com>
2024-01-29 01:07:57 +0000

    mavbar e second layout

--- .NET 6/DataLayer/obj/Debug/net6.0/DataLayer.AssemblyInfo.cs
+++ .NET 6/DataLayer/obj/Debug/net6.0/DataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070d385b3e01331aceef5c543d69851ae929bc4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
