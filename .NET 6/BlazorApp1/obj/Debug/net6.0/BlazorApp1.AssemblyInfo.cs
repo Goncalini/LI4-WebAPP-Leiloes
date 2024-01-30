b275d2b81e108057193b92919978fc7b986f6ac3
MartaRodrigues03 <martarodrigues2003@gmail.com>
2024-01-30 12:19:45 +0000

    aval e melhorias leiloes

--- .NET 6/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
+++ .NET 6/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d1782df3e5b56702e90de2e9c66cbebc62d680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
